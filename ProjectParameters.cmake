9baf86b68d19ef421945e8ba7a9190a3df1fe4ab
Giannis Tsagkatakis <jtsagata@gmail.com>
2019-08-10 10:40:33 +0300

    Customizable README.md

--- ProjectParameters.cmake
+++ ProjectParameters.cmake
@@ -2,3 +2,5 @@ set(VERSION 0.0.1)
 set(DESCRIPTION "A cpp cmake boilerplate project")
 set(REPOSITORY_URL "https://github.com/jtsagata/cpp-boiler")
 set(PROJECT_HOME_URL "https://jtsagata.github.io/cpp-boiler/")
+
+set(TRAVIS_URL "https://travis-ci.org/jtsagata/cpp-boiler.svg?branch=master")
